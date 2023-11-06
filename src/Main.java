1d1232243cd9487c6bff802d44d7ff6ee8739add
keppacco <133229659+keppacco@users.noreply.github.com>
2023-11-06 07:44:30 +0100

    ADD PRODUCTS

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
         SklepRolniczy sklep = new SklepRolniczy();
 
-        // Dodajmy kilka przykładowych artykułów
+
         Warzywo pomidor = new Warzywo("Pomidor", 2.5);
         Owoc jablko = new Owoc("Jabłko", 1.0);
         Warzywo marchew = new Warzywo("Marchew", 1.8);
@@ -21,8 +21,27 @@ public class Main {
         Owoc arbuz = new Owoc("Arbuz", 4.5);
         Warzywo papryka = new Warzywo("Papryka", 2.0);
         Owoc malina = new Owoc("Malina", 2.7);
+        Warzywo kapusta = new Warzywo("Kapusta", 1.2);
+        Owoc arbuz3 = new Owoc("Arbuz", 4.5);
+        Warzywo pietruszka = new Warzywo("Pietruszka", 1.0);
+        Owoc arbuz2 = new Owoc("Arbuz", 4.0);
+        Warzywo kalafior = new Warzywo("Kalafior", 2.0);
+        Owoc winogrono = new Owoc("Winogrono", 3.5);
+        Warzywo rukola = new Warzywo("Rukola", 1.8);
+        Owoc brzoskwinia2 = new Owoc("Brzoskwinia", 3.0);
+        Warzywo burak = new Warzywo("Burak", 1.5);
+        Owoc cytryna = new Owoc("Cytryna", 2.0);
+        Warzywo kapusta2 = new Warzywo("Kapusta", 1.3);
+        Owoc mango = new Owoc("Mango", 5.0);
+        Warzywo seler = new Warzywo("Seler", 1.6);
+        Owoc ananas = new Owoc("Ananas", 4.0);
+        Warzywo szparagi = new Warzywo("Szparagi", 2.5);
+        Owoc truskawka = new Owoc("Truskawka", 3.2);
+        Warzywo rzodkiewka = new Warzywo("Rzodkiewka", 1.0);
+        Owoc papaja = new Owoc("Papaja", 4.5);
+        Warzywo por = new Warzywo("Por", 2.2);
 
-        // Dodajmy je do sklepu
+
         sklep.dodajArtykul(pomidor, 100);
         sklep.dodajArtykul(jablko, 50);
         sklep.dodajArtykul(marchew, 75);
@@ -33,8 +52,27 @@ public class Main {
         sklep.dodajArtykul(arbuz, 30);
         sklep.dodajArtykul(papryka, 80);
         sklep.dodajArtykul(malina, 45);
+        sklep.dodajArtykul(kapusta, 80);
+        sklep.dodajArtykul(arbuz, 35);
+        sklep.dodajArtykul(pietruszka, 60);
+        sklep.dodajArtykul(arbuz2, 40);
+        sklep.dodajArtykul(kalafior, 70);
+        sklep.dodajArtykul(winogrono, 45);
+        sklep.dodajArtykul(rukola, 55);
+        sklep.dodajArtykul(brzoskwinia2, 30);
+        sklep.dodajArtykul(burak, 75);
+        sklep.dodajArtykul(cytryna, 50);
+        sklep.dodajArtykul(kapusta2, 65);
+        sklep.dodajArtykul(mango, 25);
+        sklep.dodajArtykul(seler, 85);
+        sklep.dodajArtykul(ananas, 20);
+        sklep.dodajArtykul(szparagi, 95);
+        sklep.dodajArtykul(truskawka, 42);
+        sklep.dodajArtykul(rzodkiewka, 68);
+        sklep.dodajArtykul(papaja, 22);
+        sklep.dodajArtykul(por, 88);
 
-        // Dodajmy kolejne przykładowe artykuły
+
         Warzywo ziemniak = new Warzywo("Ziemniak", 1.5);
         Owoc porzeczka = new Owoc("Porzeczka", 3.2);
         Warzywo szpinak = new Warzywo("Szpinak", 2.3);
@@ -42,7 +80,7 @@ public class Main {
         Warzywo baklazan = new Warzywo("Bakłażan", 2.1);
         Owoc pomarancza = new Owoc("Pomarańcza", 2.9);
 
-        // Dodajmy kolejne artykuły do sklepu
+
         sklep.dodajArtykul(ziemniak, 120);
         sklep.dodajArtykul(porzeczka, 55);
         sklep.dodajArtykul(szpinak, 70);
@@ -50,17 +88,17 @@ public class Main {
         sklep.dodajArtykul(baklazan, 85);
         sklep.dodajArtykul(pomarancza, 100);
 
-        // Usuńmy kilka artykułów
+
         sklep.usunArtykul("Arbuz");
         sklep.usunArtykul("Cebula");
 
-        // Znajdźmy i wyświetlmy informacje o jednym z artykułów
+
         ArtykulRolniczy znalezionyArtykul = sklep.znajdzArtykul("Ziemniak");
         if (znalezionyArtykul != null) {
             System.out.println("Znaleziono artykuł: " + znalezionyArtykul.getNazwa());
         }
 
-        // Posortujmy artykuły alfabetycznie
+
         sklep.sortujArtykuly();
         List<ArtykulRolniczy> posortowaneArtykuly = sklep.pobierzArtykuly();
         System.out.println("Posortowane artykuły:");
